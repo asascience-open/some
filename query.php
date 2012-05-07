3a13b1caed501d960f9a41fc5e4b26455480693e
Charlton Galvarino <charlton@2creek.com>
2012-05-07 14:16:58 -0400

    bbox

--- query.php
+++ query.php
@@ -130,22 +130,22 @@
       ,'varName'  => 'Sea surface elevation'
       ,'varUnits' => 'm'
       ,'abstract' => 'No information available.'
-      ,'bbox'    => '-180,-90,180,90'
+      ,'bbox'    => '-100,22,-80,32'
     ));
 
     array_push($grids,array(
        'name' => 'grid.SLOSH - Ike - GOM : Water Surface Height Above Reference Datum'
-      ,'url'  => 'http://testbedapps.sura.org/thredds/wms/inundation/mdl/slosh/ike/gom?COLORSCALERANGE=-0.5,1.0'
+      ,'url'  => 'http://testbedapps.sura.org/thredds/wms/inundation/mdl/slosh/ike/gom?COLORSCALERANGE=-1.5,1.5'
       ,'lyr'  => 'eta'
       ,'stl'  => ''
       ,'sgl'  => true
-      ,'leg'  => 'http://testbedapps.sura.org/thredds/wms/inundation/mdl/slosh/ike/gom?TRANSPARENT=true&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetLegendGraphic&EXCEPTIONS=application/vnd.ogc.se_xml&FORMAT=image/gif&LAYER=eta&TIME=&COLORSCALERANGE=-0.5,1.0'
+      ,'leg'  => 'http://testbedapps.sura.org/thredds/wms/inundation/mdl/slosh/ike/gom?TRANSPARENT=true&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetLegendGraphic&EXCEPTIONS=application/vnd.ogc.se_xml&FORMAT=image/gif&LAYER=eta&TIME=&COLORSCALERANGE=-1.5,1.5'
       ,'minT' => strtotime('2008-09-11T00:00:00.000Z')
       ,'maxT' => strtotime('2008-09-13T22:00:00.000Z')
       ,'varName'  => 'Sea surface elevation'
       ,'varUnits' => 'm'
       ,'abstract' => 'No information available.'
-      ,'bbox'    => '-180,-90,180,90'
+      ,'bbox'    => '-100,22,-80,32'
     ));
 
     array_push($grids,array(
