32fe459e41e78ce3eee7456b6aee2626889df731
cgalvarino <cgalvarino@ubuntu.(none)>
2013-03-22 11:53:26 -0400

    gif working for csv

--- getFeatureInfo.php
+++ getFeatureInfo.php
@@ -1,12 +1,13 @@
 <?php
   date_default_timezone_set('UTC');
 
-  $rawData = file_get_contents(substr($_SERVER["REQUEST_URI"],strpos($_SERVER["REQUEST_URI"],'?')+1));
+  $gfiUrl  = substr($_SERVER["REQUEST_URI"],strpos($_SERVER["REQUEST_URI"],'?')+1);
+  $rawData = file_get_contents($gfiUrl);
   $xml = @simplexml_load_string($rawData);
 
   $mapTime = $_REQUEST['mapTime'] != 'undefined' ? $_REQUEST['mapTime'] : '';
 
-  $data = array();
+  $data      = array();
   $data['t'] = array();
   $data['u'] = array();
   $data['d'] = array();
@@ -57,17 +58,17 @@
     }
   }
   else if (!$xml) {
-    $csv = csv_to_array($rawData);
+    $gfiUrl = str_replace('text%2Fxml','text%2Fcsv',$gfiUrl);
+    $csv = csv_to_array(file_get_contents($gfiUrl));
     for ($i = 0; $i < count($csv); $i++) {
-      // round to nearest hour
-      preg_match("/(\d\d\d\d)(\d\d)(\d\d)T(\d\d)(\d\d)(\d\d)Z/",$csv[$i]['time'],$a);
-      $t = mktime($a[4],0,0,$a[2],$a[3],$a[1]) + ($a[4] >= 30 ? 3600 : 0);
+      preg_match("/(\d\d\d\d)-(\d\d)-(\d\d)T(\d\d):(\d\d):(\d\d)Z/",$csv[$i]['time'],$a);
+      $t = mktime($a[4],$a[5],$a[6],$a[2],$a[3],$a[1]);
       if ($mapTime == $t) {
         $data['nowIdx'] = count($data['t']);
       }
       array_push($data['t'],$t * 1000);
       foreach (array_keys($csv[$i]) as $vStr) {
-        if ($vStr != 'time') {
+        if (preg_match('/'.$_REQUEST['varName'].'/i',$vStr)) {
           preg_match("/(.*)\[(.*)\]/",$vStr,$a);
           if (!array_key_exists($a[1],$data['d'])) {
             $data['d'][$a[1]] = array(sprintf("%f",$csv[$i][$vStr]));
@@ -107,6 +108,7 @@
     }
   }
 
+  $data['gfi'] = $gfiUrl;
   echo json_encode($data);
 
   // from http://www.php.net/manual/en/function.str-getcsv.php#100579
